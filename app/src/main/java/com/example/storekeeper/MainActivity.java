e91dfcb9454227e98e4ec93002cee4e50730d3d8
patpan7 <patpan7@gmail.com>
2023-05-19 21:31:33 +0300

    Login function done! Logout function done!

--- app/src/main/java/com/example/storekeeper/MainActivity.java
+++ app/src/main/java/com/example/storekeeper/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.storekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.storekeeper.DBClasses.DBHelper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     CardView cardProducts;
@@ -14,22 +28,33 @@ public class MainActivity extends AppCompatActivity {
     CardView cardCharge;
     CardView cardReturns;
     CardView cardWarranty;
-    CardView cardSettings;
+    CardView cardLogout;
 
+    TextView username;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("MyAppName", MODE_PRIVATE);
+        if (sharedPreferences.getString("logged", "false").equals(false)) {
+            Intent intent = new Intent(MainActivity.this, login.class);
+            startActivity(intent);
+            finish();
+        }
+        username = findViewById(R.id.main_username);
+        username.append(sharedPreferences.getString("username",""));
 
-        cardProducts  = findViewById(R.id.cardProducts);
-        cardEmployees  = findViewById(R.id.cardEmployees);
-        cardSupplier  = findViewById(R.id.cardSupplier);
-        cardIncome  = findViewById(R.id.cardIncome);
-        cardCharge  = findViewById(R.id.cardCharge);
-        cardReturns  = findViewById(R.id.cardReturns);
-        cardWarranty  = findViewById(R.id.cardWarranty);
-        cardSettings  = findViewById(R.id.cardSettings);
+        cardProducts = findViewById(R.id.cardProducts);
+        cardEmployees = findViewById(R.id.cardEmployees);
+        cardSupplier = findViewById(R.id.cardSupplier);
+        cardIncome = findViewById(R.id.cardIncome);
+        cardCharge = findViewById(R.id.cardCharge);
+        cardReturns = findViewById(R.id.cardReturns);
+        cardWarranty = findViewById(R.id.cardWarranty);
+        cardLogout = findViewById(R.id.cardLogout);
 
         cardProducts.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, products.class);
@@ -73,9 +98,42 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        cardSettings.setOnClickListener(view -> {
-//            Intent intent = new Intent(MainActivity.this, settings.class);
-//            startActivity(intent);
+        cardLogout.setOnClickListener(view -> {
+            DBHelper helper = new DBHelper(MainActivity.this);
+            String ip = helper.getSettingsIP();
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            String url = "http://" + ip + "/storekeeper/logout.php";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equals("success")) {
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("logged", "");
+                                editor.putString("username", "");
+                                editor.putString("apiKey", "");
+                                editor.apply();
+                                Intent intent = new Intent(MainActivity.this, login.class);
+                                startActivity(intent);
+                                finish();
+                            } else
+                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                   error.printStackTrace();
+                }
+            }) {
+                protected Map<String, String> getParams() {
+                    Map<String, String> paramV = new HashMap<>();
+                    paramV.put("username", sharedPreferences.getString("username",""));
+                    paramV.put("apiKey", sharedPreferences.getString("apiKey",""));
+                    return paramV;
+                }
+            };
+            queue.add(stringRequest);
+
 
         });
 
--- app/src/main/java/com/example/storekeeper/login.java
+++ app/src/main/java/com/example/storekeeper/login.java
@@ -1,24 +1,54 @@
 package com.example.storekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.storekeeper.DBClasses.DBHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
     Button login;
     CardView cardSettings;
     EditText username, password;
     ProgressBar progressBar;
+    String usernameAPI, apiKey;
+    String usernameData, passwordData;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        sharedPreferences = getSharedPreferences("MyAppName", MODE_PRIVATE);
+        if (sharedPreferences.getString("logged", "false").equals(true)) {
+            Intent intent = new Intent(login.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        DBHelper helper = new DBHelper(login.this);
 
         username = findViewById(R.id.login_username1);
         password = findViewById(R.id.login_password1);
@@ -28,7 +58,58 @@ public class login extends AppCompatActivity {
         login.setOnClickListener(view -> {
 //            Intent intent = new Intent(login.this, MainActivity.class);
 //            startActivity(intent);
-
+            progressBar.setVisibility(View.VISIBLE);
+            String ip = helper.getSettingsIP();
+            usernameData = String.valueOf(username.getText());
+            passwordData = String.valueOf(password.getText());
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            String url = "http://" + ip + "/storekeeper/login.php";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            progressBar.setVisibility(View.GONE);
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                String status = jsonObject.getString("status");
+                                String message = jsonObject.getString("message");
+                                if (status.equals("success")) {
+                                    usernameAPI = jsonObject.getString("username");
+                                    apiKey = jsonObject.getString("apiKey");
+                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("logged", "true");
+                                    editor.putString("username", usernameAPI);
+                                    editor.putString("apiKey", apiKey);
+                                    editor.apply();
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                        username.setError("Error!!!");
+                                        password.setError("Error!!!");
+                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            }) {
+                protected Map<String, String> getParams() {
+                    Map<String, String> paramV = new HashMap<>();
+                    paramV.put("username", usernameData);
+                    paramV.put("password", passwordData);
+                    return paramV;
+                }
+            };
+            queue.add(stringRequest);
         });
 
         cardSettings = findViewById(R.id.cardSettings);
