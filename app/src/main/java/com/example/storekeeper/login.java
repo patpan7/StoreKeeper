0e5915b1e1ec5a5a1cc7ade422354c916a30f19d
Lab <patpan7@gmail.com>
2023-05-19 15:15:52 +0300

    login in progress

--- app/src/main/java/com/example/storekeeper/login.java
+++ app/src/main/java/com/example/storekeeper/login.java
@@ -3,6 +3,8 @@ package com.example.storekeeper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -10,16 +12,22 @@ import androidx.cardview.widget.CardView;
 public class login extends AppCompatActivity {
     Button login;
     CardView cardSettings;
+    EditText username, password;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username = findViewById(R.id.login_username1);
+        password = findViewById(R.id.login_password1);
+        progressBar = findViewById(R.id.loading);
+
         login = findViewById(R.id.loginButton);
         login.setOnClickListener(view -> {
-            Intent intent = new Intent(login.this, MainActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(login.this, MainActivity.class);
+//            startActivity(intent);
 
         });
 
